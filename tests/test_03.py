203a2cc7c4eae8cd9a317af1b77c7816e5ddd296
Joel Petersson <joel.pettersson@gmail.com>
2018-12-18 16:15:17 +0100

    fixing typo in test message

--- tests/test_03.py
+++ tests/test_03.py
@@ -14,7 +14,7 @@ def test_part_1_sample_input():
 def test_part_1_real_input():
     my_input = p03.get_input_from_file(input_file_path)
     number_of_patches = len(p03.get_intersection_patches(my_input))
-    assert_equal(number_of_patches, 121163, f"expected 4 overlapping patches, but got {number_of_patches}")
+    assert_equal(number_of_patches, 121163, f"expected 121163 overlapping patches, but got {number_of_patches}")
 
 
 def test_part_2_sample_input():
@@ -25,4 +25,4 @@ def test_part_2_sample_input():
 def test_part_2_real_input():
     my_input = p03.get_input_from_file(input_file_path)
     non_overlapping_id = p03.get_non_intersecting_patch(my_input)
-    assert_equal(non_overlapping_id, 943, f"expected id 3 but got {non_overlapping_id}")
+    assert_equal(non_overlapping_id, 943, f"expected id 943 but got {non_overlapping_id}")
