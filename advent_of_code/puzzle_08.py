8249b3fdd8b3e6b64fa41d2ff57883f6efb8a036
Joel Petersson <joel.pettersson@gmail.com>
2018-12-21 09:51:40 +0100

    Fixing missunderstanding of puzzle 8 part 1

--- advent_of_code/puzzle_08.py
+++ advent_of_code/puzzle_08.py
@@ -1,7 +1,47 @@
-def sum_metadata(meta_data_inp):
-    meta_data = meta_data_inp.strip().split(" ")
-    sum = 0
-    for m in meta_data:
-        sum += int(m)
-    return sum
+class Tree:
+    def __init__(self):
+        self.children = list()
+        self.metadata = list()
 
+    def add_child(self, child):
+        self.children.append(child)
+
+    def add_metadata(self, meta):
+        self.metadata.append(meta)
+
+    def get_children(self):
+        return self.children
+
+    def get_metadata(self):
+        return self.metadata
+
+
+def build_tree(tree_def_inp):
+    tree_def = tree_def_inp.strip().split(" ")
+    tree_def = [int(t) for t in tree_def]
+    idx, tree_head = parse_tree(tree_def, 0)
+    return tree_head
+
+
+def parse_tree(tree_def, idx):
+    num_childs = tree_def[idx]
+    idx += 1
+    num_metadata = tree_def[idx]
+    idx += 1
+    tree = Tree()
+    for i in range(num_childs):
+        idx, child = parse_tree(tree_def, idx)
+        tree.add_child(child)
+    for i in range(idx, idx + num_metadata):
+        tree.add_metadata(tree_def[i])
+    idx += num_metadata
+    return idx, tree
+
+
+def sum_metadata(tree_head):
+    sum = 0
+    for c in tree_head.get_children():
+        sum += sum_metadata(c)
+    for meta in tree_head.get_metadata():
+        sum += meta
+    return sum
--- tests/test_08.py
+++ tests/test_08.py
@@ -2,17 +2,19 @@ import advent_of_code.puzzle_08 as p08
 from nose.tools import assert_equal
 import os
 
-sample_input_meta_data = "1 1 2 10 11 12 2 99"
+sample_input_meta_data = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
 input_file_path = os.path.join(os.path.dirname(__file__), "..", "inputs", "08_input.txt")
 
 
 def test_sum_metadata_sample():
-    sum = p08.sum_metadata(sample_input_meta_data)
-    assert_equal(sum, 138, f"expected sum to be 138, but was {sum}")
+    tree_head = p08.build_tree(sample_input_meta_data)
+    meta_sum = p08.sum_metadata(tree_head)
+    assert_equal(meta_sum, 138, f"expected meta_sum to be 138, but was {meta_sum}")
 
 
 def test_sum_metadata():
     with open(input_file_path) as f:
         meta_data_inp = f.read().strip()
-    sum = p08.sum_metadata(meta_data_inp)
-    assert_equal(sum, 49659, f"expected sum to be 49659, but was {sum}")
+    tree_head = p08.build_tree(meta_data_inp)
+    meta_sum = p08.sum_metadata(tree_head)
+    assert_equal(meta_sum, 36891, f"expected meta_sum to be 36891, but was {meta_sum}")
