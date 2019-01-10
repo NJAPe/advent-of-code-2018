79783d4ec3e7ec29be76d6231e26d3cf7199dfc8
Joel Petersson <joel.pettersson@gmail.com>
2019-01-10 22:02:40 +0100

    Finalizing puzzle 17

--- advent_of_code/puzzle_17.py
+++ advent_of_code/puzzle_17.py
@@ -47,7 +47,7 @@ def parse_input(raw_input):
                 topografy[y][x] = "#"
     # Add well
     topografy[0][500-x_offset] = "+"
-    return x_offset, topografy
+    return x_offset, min_y, max_y, topografy
 
 
 def print_topografy(topografy):
@@ -57,3 +57,75 @@ def print_topografy(topografy):
             str += char
         str += "\n"
     print(str)
+
+
+DOWN = 0
+LEFT = 1
+RIGHT = 2
+UP = 3
+RET_HIT_WALL = 4
+RET_FULL = 5
+RET_DONE = 6
+def fill_water(x, y, direction, fill_type, topo):
+    if direction == DOWN:
+        y += 1
+    elif direction == UP:
+        y -= 1
+    elif direction == LEFT:
+        x -= 1
+    elif direction == RIGHT:
+        x += 1
+    else:
+        pass # Unknown direction
+
+    if y >= len(topo):
+        return RET_DONE
+    if topo[y][x] == "#":
+        return RET_HIT_WALL
+    elif topo[y][x] == "~":
+        return RET_FULL
+    topo[y][x] = fill_type
+    if y + 1 >= len(topo):
+        return fill_water(x, y, DOWN, "|", topo)
+    if topo[y+1][x] == "#" or topo[y+1][x] == "~":
+        # non-penetrable surface
+        if direction != DOWN:
+            return fill_water(x, y, direction, fill_type, topo)
+        else:
+            # spread in right and left direction
+            ret_right = fill_water(x, y, RIGHT, fill_type, topo)
+            ret_left = fill_water(x, y, LEFT, fill_type, topo)
+            if (ret_right == RET_HIT_WALL or ret_right == RET_FULL) and (ret_left == RET_HIT_WALL or ret_left == RET_FULL):
+                topo[y][x] = "~"
+                fill_water(x, y, RIGHT, "~", topo)
+                fill_water(x, y, LEFT, "~", topo)
+                return RET_FULL
+    else:
+        # Penetrable surface
+        ret = fill_water(x, y, DOWN, "|", topo)
+        if ret == RET_FULL:
+            # spread in right and left direction
+            ret_right = fill_water(x, y, RIGHT, "|", topo)
+            ret_left = fill_water(x, y, LEFT, "|", topo)
+            if ret_right == RET_HIT_WALL and ret_left == RET_HIT_WALL:
+                topo[y][x] = "~"
+                fill_water(x, y, RIGHT, "~", topo)
+                fill_water(x, y, LEFT, "~", topo)
+                return RET_FULL
+            elif ret_right == RET_FULL or ret_left == RET_FULL:
+                return RET_FULL
+
+
+def analyze_well(raw_input):
+    x_offset, min_y, max_y, topo = parse_input(raw_input)
+    fill_water(500 - x_offset, 0, DOWN, "|", topo)
+    still = 0
+    flowing = 0
+    for y in range(min_y, max_y+1):
+        line = topo[y]
+        for char in line:
+            if char == "|":
+                flowing += 1
+            elif char == "~":
+                still += 1
+    return still, flowing
--- tests/test_17.py
+++ tests/test_17.py
@@ -1,6 +1,7 @@
 import advent_of_code.puzzle_17 as p17
 from nose.tools import assert_equal, assert_true, assert_false
 import os
+import sys
 
 
 sample_input = """x=495, y=2..7
@@ -10,7 +11,8 @@ x=498, y=2..4
 x=506, y=1..2
 x=498, y=10..13
 x=504, y=10..13
-y=13, x=498..504"""
+y=13, x=498..504
+y=11, x=501..502"""
 sample_topo = [[".", ".", ".", ".", ".", ".", "+", ".", ".", ".", ".", ".", ".", "."],
                [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "#", "."],
                [".", "#", ".", ".", "#", ".", ".", ".", ".", ".", ".", ".", "#", "."],
@@ -22,7 +24,7 @@ sample_topo = [[".", ".", ".", ".", ".", ".", "+", ".", ".", ".", ".", ".", ".",
                [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
                [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
                [".", ".", ".", ".", "#", ".", ".", ".", ".", ".", "#", ".", ".", "."],
-               [".", ".", ".", ".", "#", ".", ".", ".", ".", ".", "#", ".", ".", "."],
+               [".", ".", ".", ".", "#", ".", ".", "#", "#", ".", "#", ".", ".", "."],
                [".", ".", ".", ".", "#", ".", ".", ".", ".", ".", "#", ".", ".", "."],
                [".", ".", ".", ".", "#", "#", "#", "#", "#", "#", "#", ".", ".", "."]]
 sample_topo_complete = [[".", ".", ".", ".", ".", ".", "+", ".", ".", ".", ".", ".", ".", "."],
@@ -36,13 +38,38 @@ sample_topo_complete = [[".", ".", ".", ".", ".", ".", "+", ".", ".", ".", ".", 
                [".", ".", ".", ".", ".", ".", ".", ".", "|", ".", ".", ".", ".", "."],
                [".", ".", ".", "|", "|", "|", "|", "|", "|", "|", "|", "|", ".", "."],
                [".", ".", ".", "|", "#", "~", "~", "~", "~", "~", "#", "|", ".", "."],
-               [".", ".", ".", "|", "#", "~", "~", "~", "~", "~", "#", "|", ".", "."],
+               [".", ".", ".", "|", "#", "~", "~", "#", "#", "~", "#", "|", ".", "."],
                [".", ".", ".", "|", "#", "~", "~", "~", "~", "~", "#", "|", ".", "."],
                [".", ".", ".", "|", "#", "#", "#", "#", "#", "#", "#", "|", ".", "."]]
+input_file_path = os.path.join(os.path.dirname(__file__), "..", "inputs", "17_input.txt")
 
 
-def test_parse_input():
-    x_offset, topo = p17.parse_input(sample_input)
+def test_parse_input_sample_topo():
+    x_offset, min_y, max_y, topo = p17.parse_input(sample_input)
     assert_equal(x_offset, 494, "Wrong offset")
+    assert_equal(min_y, 1, "Wrong min_y")
+    assert_equal(max_y, 13, "wrong max_y")
     for y in range(len(sample_topo)):
         assert_equal(topo[y], sample_topo[y], f"Wrong topografy on y={y}")
+
+
+def test_fill_water_sample_topo():
+    x_offset, min_y, max_y, topo = p17.parse_input(sample_input)
+    p17.fill_water(500 - x_offset, 0, p17.DOWN, "|", topo)
+    for y in range(len(sample_topo_complete)):
+        assert_equal(topo[y], sample_topo_complete[y], f"Wrong topografy on y={y}")
+
+
+def test_part_1_sample_topo():
+    still, flowing = p17.analyze_well(sample_input)
+    assert_equal(still + flowing, 55)
+
+
+def test_part_1_real():
+    sys.setrecursionlimit(15000)
+    with open(input_file_path) as f:
+        raw_input = f.read().strip()
+    still, flowing = p17.analyze_well(raw_input)
+    assert_equal(still + flowing, 31412, "Wrong number of water")
+    assert_equal(still, 25857, "Wrong number of still water")
+    assert_equal(flowing, 5555, "Wrong number of flowing water")
